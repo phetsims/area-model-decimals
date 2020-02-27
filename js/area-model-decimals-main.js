624b5f5f2976ff5b817d886c2256267cf4318805
samreid <reids@colorado.edu>
2020-02-27 11:38:05 -0700

    Add repo to migration list, see https://github.com/phetsims/chipper/issues/875

--- js/area-model-decimals-main.js
+++ js/area-model-decimals-main.js
@@ -5,29 +5,25 @@
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
-define( require => {
-  'use strict';
 
-  // modules
-  const DecimalsScreen = require( 'AREA_MODEL_COMMON/screens/DecimalsScreen' );
-  const Sim = require( 'JOIST/Sim' );
-  const SimLauncher = require( 'JOIST/SimLauncher' );
+import DecimalsScreen from '../../area-model-common/js/screens/DecimalsScreen.js';
+import Sim from '../../joist/js/Sim.js';
+import SimLauncher from '../../joist/js/SimLauncher.js';
+import areaModelDecimalsStrings from './area-model-decimals-strings.js';
 
-  // strings
-  const areaModelDecimalsTitleString = require( 'string!AREA_MODEL_DECIMALS/area-model-decimals.title' );
+const areaModelDecimalsTitleString = areaModelDecimalsStrings[ 'area-model-decimals' ].title;
 
-  const simOptions = {
-    credits: {
-      leadDesign: 'Amy Hanson, Amanda McGarry',
-      softwareDevelopment: 'Jonathan Olson',
-      team: 'Karina Hensberry, Susan Miller, Ariel Paul, Kathy Perkins, Oliver Nix',
-      qualityAssurance: 'Steele Dalton, Bryce Griebenow, Ethan Johnson, Liam Mulhall, Ben Roberts, Jacob Romero, Ethan Ward, Clara Wilson, Kathryn Woessner',
-      graphicArts: 'Mariah Hermsmeyer, Amanda McGarry, Diana L\u00f3pez Tavares'
-    }
-  };
+const simOptions = {
+  credits: {
+    leadDesign: 'Amy Hanson, Amanda McGarry',
+    softwareDevelopment: 'Jonathan Olson',
+    team: 'Karina Hensberry, Susan Miller, Ariel Paul, Kathy Perkins, Oliver Nix',
+    qualityAssurance: 'Steele Dalton, Bryce Griebenow, Ethan Johnson, Liam Mulhall, Ben Roberts, Jacob Romero, Ethan Ward, Clara Wilson, Kathryn Woessner',
+    graphicArts: 'Mariah Hermsmeyer, Amanda McGarry, Diana L\u00f3pez Tavares'
+  }
+};
 
-  SimLauncher.launch( function() {
-    const sim = new Sim( areaModelDecimalsTitleString, [ new DecimalsScreen() ], simOptions );
-    sim.start();
-  } );
+SimLauncher.launch( function() {
+  const sim = new Sim( areaModelDecimalsTitleString, [ new DecimalsScreen() ], simOptions );
+  sim.start();
 } );
