43852ec6030acb3b4fe6b7db6f21ac65fbc8f9d7
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:39 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for area-model-decimals.
+ * ESLint configuration for area-model-decimals.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
